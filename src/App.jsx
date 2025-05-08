6e1435b6d5cba2ca26c05387884221180b11146d
Đức Mạnh <manhwing2005@gmail.com>
2025-05-08 13:51:38 +0700

    Khởi tạo dự án, import thư viện

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TodoList from "./assets/components/TodoList";
+import Textfield from "@atlaskit/textfield";
+import Button from "@atlaskit/button";
+
+function App() {
+  return (
+    <h3>danh sách cần làm</h3>
+  );
+}
+
+export default App;
---
+++ src/assets/components/TodoList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TodoList() {
+  return <p>This is TodoList</p>;
+}
